981b558f4a915f27dab1e65aa769a7b6101d6fde
abdulshakur <abdulshakur486@gmail.com>
2022-05-13 19:56:41 +0300

    sidebar completed.

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,70 @@
+import React, { useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { BackdropWrapper, SidebarWrapper } from "./SidebarStyle";
+import { isFirefox, isIE } from "react-device-detect";
+import { ToggleContext } from "../../pages/Layout/Layout";
+
+function Sidebar() {
+  const { pathname } = useLocation();
+  const { setToggle } = useContext(ToggleContext);
+
+  const handleClick = () => {
+    setToggle((prev) => {
+      return { ...prev, burger: false };
+    });
+  };
+
+  return (
+    <>
+      <SidebarWrapper
+        initial={{ x: "100vh" }}
+        animate={{ x: 0 }}
+        exit={{ x: "100vh" }}
+        transition={{ damping: false }}
+        style={
+          isFirefox || isIE
+            ? { backgroundColor: "rgb(12, 14, 24)" }
+            : { backdropFilter: "blur(81.5485px)" }
+        }
+      >
+        <div className="container">
+          <div className="content">
+            <div className="link">
+              <Link onClick={handleClick} to={"/"}>
+                <em>00</em>Home
+              </Link>
+              <div className={`block ${pathname === "/"}`} />
+            </div>
+            <div className="link">
+              <Link onClick={handleClick} to={"/destinations"}>
+                <em>01</em>Destinations
+              </Link>
+              <div className={`block ${pathname === "/destinations"}`} />
+            </div>
+            <div className="link">
+              <Link onClick={handleClick} to={"/crew"}>
+                <em>02</em>Crew
+              </Link>
+              <div className={`block ${pathname === "/crew"}`} />
+            </div>
+            <div className="link">
+              <Link onClick={handleClick} to={"/technology"}>
+                <em>03</em>Technology
+              </Link>
+              <div className={`block ${pathname === "/technology"}`} />
+            </div>
+          </div>
+        </div>
+      </SidebarWrapper>
+      <BackdropWrapper
+        onClick={() =>
+          setToggle((prev) => {
+            return { ...prev, burger: false };
+          })
+        }
+      />
+    </>
+  );
+}
+
+export default Sidebar;
