4147a423583adea5a71e138693884712537e1fb9
abdulshakur <abdulshakur486@gmail.com>
2022-05-17 18:10:52 +0300

    completed.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { HomeWrapper } from "./HomeStyle";
+import { H1, H2, P } from "../../styles/";
+import { useNavigate } from "react-router-dom";
 
 function Home({ duration }) {
+  const navigator = useNavigate();
   return (
     <HomeWrapper
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration }}
-    ></HomeWrapper>
+    >
+      <div className="container">
+        <H2>SO, YOU WANT TO TRAVEL TO</H2>
+        <H1>SPACE</H1>
+        <P>
+          Let's face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we'll give you a truly out of this world
+          experience!
+        </P>
+      </div>
+      <div className="button_container">
+        <button onClick={() => navigator("/destinations")}>EXPOLORE</button>
+      </div>
+    </HomeWrapper>
   );
 }
 
