17cd2e796025cb508fb38ae2143d8f2fb2fcf9ef
abdulshakur <abdulshakur486@gmail.com>
2022-05-19 19:43:20 +0300

    added throttle function.

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,5 +1,31 @@
 import { useEffect, useState } from "react";
 
+function throttle(cb, delay = 50) {
+  let shouldWait = false;
+  let waitingArgs;
+
+  const timeoutFunc = () => {
+    if (waitingArgs === null) {
+      shouldWait = false;
+    } else {
+      cb(waitingArgs);
+      waitingArgs = null;
+      setTimeout(timeoutFunc, delay);
+    }
+  };
+
+  return (...args) => {
+    if (shouldWait) {
+      waitingArgs = args;
+      return;
+    }
+    cb(...args);
+    shouldWait = true;
+
+    setTimeout(timeoutFunc, delay);
+  };
+}
+
 const useScroll = () => {
   const [data, setData] = useState({
     currentY: 0,
@@ -16,9 +42,11 @@ const useScroll = () => {
       });
     };
 
-    window.addEventListener("scroll", handleScroll);
+    const updateThrottle = throttle(handleScroll);
 
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", updateThrottle);
+
+    return () => window.removeEventListener("scroll", updateThrottle);
   }, []);
 
   return data;
