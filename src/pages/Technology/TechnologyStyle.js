12594c229c4225d667795f9c2eb68ac6bb9dda84
abdulshakur <abdulshakur486@gmail.com>
2022-05-25 14:58:59 +0300

    removed unnecessary code.

--- src/pages/Technology/TechnologyStyle.js
+++ src/pages/Technology/TechnologyStyle.js
@@ -4,36 +4,6 @@ import backgroundMobile from "../../assets/technology/background-technology-mobi
 import backgroundTablet from "../../assets/technology/background-technology-tablet.jpg";
 import backgroundDesktop from "../../assets/technology/background-technology-desktop.jpg";
 
-const technologies = [
-  {
-    name: "Launch vehicle",
-    images: {
-      portrait: require("../../assets/technology/image-launch-vehicle-portrait.jpg"),
-      landscape: require("../../assets/technology/image-launch-vehicle-landscape.jpg"),
-    },
-    description:
-      "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
-  },
-  {
-    name: "Spaceport",
-    images: {
-      portrait: require("../../assets/technology/image-spaceport-portrait.jpg"),
-      landscape: require("../../assets/technology/image-spaceport-landscape.jpg"),
-    },
-    description:
-      "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
-  },
-  {
-    name: "Space capsule",
-    images: {
-      portrait: require("../../assets/technology/image-space-capsule-portrait.jpg"),
-      landscape: require("../../assets/technology/image-space-capsule-landscape.jpg"),
-    },
-    description:
-      "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
-  },
-];
-
 export const TechnologyWrapper = styled(motion.div)`
   min-height: 100vh;
   padding: 5.5rem 0 0 0;
@@ -289,9 +259,6 @@ export const ContentWrapper = styled(motion.div)`
 
 export const IMG = styled(motion.img)`
   background-size: cover;
-  /* background-image: ${(props) => {
-    return `url(${technologies[props.currentindex].images.landscape})`;
-  }}; */
   width: 100%;
   height: 170px;
 
@@ -303,8 +270,5 @@ export const IMG = styled(motion.img)`
   @media ${(props) => props.theme.breakpoints.lg} {
     height: 527px;
     max-width: 515px;
-    /* background-image: ${(props) => {
-      return `url(${technologies[props.currentindex].images.portrait})`;
-    }}; */
   }
 `;
